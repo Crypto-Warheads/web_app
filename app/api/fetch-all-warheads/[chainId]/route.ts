05fcd91851692b0112ea8007ab76b5685900bc8e
Yash Goyal <yashgo0018@gmail.com>
2024-06-02 10:40:33 +0200

    integrated the subgraph for linea

--- app/api/fetch-all-warheads/[chainId]/route.ts
+++ app/api/fetch-all-warheads/[chainId]/route.ts
@@ -3,6 +3,37 @@ import axios from "axios";
 
 export const GET = async (req: NextRequest, { params: { chainId } }: { params: { chainId: string } }) => {
     // chainId == "5000";
+    if (chainId === "59144") {
+        const { data: d } = await axios.post("https://api.studio.thegraph.com/query/77223/warheads/v0.0.1", {
+            query: `{
+                warheadClaimeds {
+                    claimer
+                    warheadId
+                    claimedAt
+                }
+                warheadCreateds {
+                    dropper
+                    warheadId
+                    warheadAddress
+                }
+                warheadCreatedWithReceivers {
+                    dropper
+                    targetReceiver
+                    warheadAddress
+                    warheadId
+                }
+                warheadDroppeds {
+                    impactTime
+                    targetLat
+                    targetLong
+                    warheadId
+                }
+            }`
+        });
+
+        return NextResponse.json({ data: d });
+    }
+
     const { data } = await axios.post("https://srv.streamingfast.io/4aec28fa/graphql", {
         query: `
         query {
